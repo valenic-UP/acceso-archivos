614aad93ddf018538971dfd73ecd68ecd12acae7
valenic-UP <0282748@up.edu.mx>
2026-03-25 13:22:39 -0600

    completo

--- main.cpp
+++ main.cpp
@@ -8,12 +8,58 @@ const int sizeF=5;
 
 int main(){
     string frases[sizeS];
-    float floatntes[sizeF];
+    float flotantes[sizeF];
 
     //pedir datos al usuario:
     cout<<"Ingrese 5 frases: "<<endl;
     for(int i =0; i<sizeS; i++){
         getline(cin, frases[i]);
+    }
+
+    cout << "Ingrese 5 números flotantes:" << endl;
+        for (int i = 0; i < sizeF; i++) {
+            cin >> flotantes[i];
+        }
+    cin.ignore(); // Limpiar buffer
+
+    // Guardar en archivo de texto
+    ofstream archivoTexto("datos.txt");
+    if (archivoTexto.is_open()) {
+        cout << "\nEscribiendo en archivo txt: " << endl;
+    for ( string frase : frases) {
+        archivoTexto << frase << endl;
+    }
+    archivoTexto.close();
+    }
+    else {
+        cout << "Error al abrir archivo de texto." << endl;
+    }
+
+    // Guardar en archivo de texto
+    ofstream archivoTexto("datos.txt");
+    if (archivoTexto.is_open()) {
+        cout << "\nEscribiendo en archivo txt: " << endl;
+    for ( float numero : flotantes) {
+        archivoTexto << numero << endl;
+    }
+        archivoTexto.close();
+    }
+    else {
+        cout << "Error al abrir archivo de texto." << endl;
+    }
+
+    // Leer desde archivo de texto
+    cout << "\nLeyendo desde archivo de texto:" << endl;
+    ifstream archivoTextoLectura("datos.txt");
+    if (archivoTextoLectura.is_open()) {
+        string linea;
+    while (getline(archivoTextoLectura, linea)) {
+        cout << linea << endl;
+    }
+        archivoTextoLectura.close();
+    }
+    else {
+        cout << "Error al leer archivo de texto." << endl;
     }
 
     return 0;
